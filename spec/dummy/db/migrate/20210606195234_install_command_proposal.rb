0c54ef53414b66d90eecd55ad49dcac305a118f5
Rocco Nicholls <rocco11nicholls@gmail.com>
2021-06-06 13:54:44 -0600

    Red spec

---
+++ spec/dummy/db/migrate/20210606195234_install_command_proposal.rb
@@ -0,0 +1,37 @@
+class InstallCommandProposal < ActiveRecord::Migration[5.2]
+  def change
+    create_table :command_proposal_tasks do |t|
+      # has_many :iterations
+      t.integer :session_type # [line, session, function]
+      t.datetime :last_executed_at
+
+      t.timestamps
+    end
+
+    create_table :command_proposal_iterations do |t|
+      # has_many :comments
+      t.belongs_to :task
+      t.text :args
+      t.text :code
+      t.text :result
+      t.integer :status # [created approved started failed success]
+      t.belongs_to :author
+      t.belongs_to :approver
+      t.datetime :approved_at
+      t.datetime :started_at
+      t.datetime :completed_at
+      t.datetime :stopped_at
+
+      t.timestamps
+    end
+
+    create_table :command_proposal_comments do |t|
+      t.belongs_to :iteration
+      t.integer :line_number
+      t.belongs_to :author
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/dummy/db/schema.rb
@@ -0,0 +1,55 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_06_06_195234) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "command_proposal_comments", force: :cascade do |t|
+    t.bigint "iteration_id"
+    t.integer "line_number"
+    t.bigint "author_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_command_proposal_comments_on_author_id"
+    t.index ["iteration_id"], name: "index_command_proposal_comments_on_iteration_id"
+  end
+
+  create_table "command_proposal_iterations", force: :cascade do |t|
+    t.bigint "task_id"
+    t.text "args"
+    t.text "code"
+    t.text "result"
+    t.integer "status"
+    t.bigint "author_id"
+    t.bigint "approver_id"
+    t.datetime "approved_at"
+    t.datetime "started_at"
+    t.datetime "completed_at"
+    t.datetime "stopped_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["approver_id"], name: "index_command_proposal_iterations_on_approver_id"
+    t.index ["author_id"], name: "index_command_proposal_iterations_on_author_id"
+    t.index ["task_id"], name: "index_command_proposal_iterations_on_task_id"
+  end
+
+  create_table "command_proposal_tasks", force: :cascade do |t|
+    t.integer "session_type"
+    t.datetime "last_executed_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
